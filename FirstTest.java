ddc7aa1c4c491624e3023c7280f9855985aadde2
Olga Negovora <olgan_85@bk.ru>
2020-09-23 08:34:06 +0300

    Урок2Ex2

--- FirstTest.java
+++ FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -28,9 +33,32 @@ public class FirstTest {
     {
         driver.quit();
     }
+
     @Test
-    public void firstTest()
+    public void testEx2()
     {
-        System.out.println("First test run");
+        assertElementHasText(
+                        By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                        "Search Wikipedia",
+                        "Cannot find the text field 'Search Wikipedia'",
+                        5
+        );
     }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
+    {
+        WebDriverWait wait=new WebDriverWait(driver,timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    public void assertElementHasText(By by, String text, String error_message, long timeoutInSeconds)
+
+        {
+            WebElement element = waitForElementPresent(by,error_message, timeoutInSeconds);
+            String actual=element.getAttribute("text");
+            Assert.assertEquals(error_message, text, actual);
+        }
 }
