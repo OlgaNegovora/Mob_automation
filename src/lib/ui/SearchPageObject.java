38d46d44f4e803ea3a781270f4918ec1ad012ecc
Olga Negovora <olgan_85@bk.ru>
2020-10-11 12:59:09 +0300

    Revert "Ex9: Рефакторинг темплейта"
    This reverts commit 01f0de27b952f8e4bf593e7afb03e798b68d80bf.

--- src/lib/ui/SearchPageObject.java
+++ src/lib/ui/SearchPageObject.java
@@ -8,12 +8,11 @@ public class SearchPageObject extends MainPageObject //методы для по�
        private static final String
        SEARCH_INIT_ELEMENT="//*[contains(@text,'Search Wikipedia')]",
        SEARCH_INPUT="//*[contains(@text,'Search…')]",
-       /*SEARCH_CANCEL_BUTTON="org.wikipedia:id/search_close_btn",
-       SEARCH_RESULT_BY_SUBSTRING_TPL ="//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",//org.wikipedia:id/page_list_item_description
+       SEARCH_CANCEL_BUTTON="org.wikipedia:id/search_close_btn",
+       SEARCH_RESULT_BY_SUBSTRING_TPL ="//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']",
        SEARCH_RESULT_ELEMENT="//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']",
        SEARCH_EMPTY_RESULT_ELEMENT="//*[@text='No results found']",
-       SEARCH_RESULT="org.wikipedia:id/page_list_item_container",*/
-       SEARCH_RESULT_BY_SUBSTRING_TITLE_DESCRIPTION_TPL ="//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{DESCRIPTION}'][@resource-id='org.wikipedia:id/page_list_item_description']/..//*[@text='{TITLE}'][@resource-id='org.wikipedia:id/page_list_item_title']";
+       SEARCH_RESULT="org.wikipedia:id/page_list_item_container";
 
        public SearchPageObject(AppiumDriver driver)
        {
@@ -21,10 +20,9 @@ public class SearchPageObject extends MainPageObject //методы для по�
        }
 
        /*TEMPLATES METHODS*/
-       private static String getResultSearchElementSubstringByTitleAndDescription(String title, String description)
+       private static String getResultSearchElement(String substring)
        {
-           return SEARCH_RESULT_BY_SUBSTRING_TITLE_DESCRIPTION_TPL.replace ("{TITLE}",title).replace("{DESCRIPTION}",description) ;
-
+           return SEARCH_RESULT_BY_SUBSTRING_TPL.replace("{SUBSTRING}",substring);
        }
        /*TEMPLATES METHODS*/
 
@@ -34,16 +32,71 @@ public class SearchPageObject extends MainPageObject //методы для по�
            this.waitForElementPresent(By.xpath(SEARCH_INIT_ELEMENT),"Cannot find search input after clicking search init element");
        }
 
+       public void waitForCancelButtonToAppear()
+       {
+           this.waitForElementPresent(By.id(SEARCH_CANCEL_BUTTON),"Cannot find search cancel button",5);
+       }
+
+       public void waitForCancelButtonToDisappear()
+       {
+           this.waitForElementNotPresent(By.id(SEARCH_CANCEL_BUTTON),"Search cancel button is still present!",5);
+       }
+
+       public void clickCancelSearch()
+       {
+           this.waitForElementAndClick(By.id(SEARCH_CANCEL_BUTTON), "Cannot find and click search cancel button.", 5);
+       }
+
        public void typeSearchLine(String search_line)
        {
            this.waitForElementAndSendKeys(By.xpath(SEARCH_INPUT), search_line, "Cannot find and type into search input",5 );
        }
 
-       public void waitForElementByTitleAndDescription(String title, String description)
+       public void waitForSearchResult(String substring)
        {
-           String search_result_xpath=getResultSearchElementSubstringByTitleAndDescription(title, description);
-           //System.out.println(search_result_xpath);
-           this.waitForElementPresent(By.xpath(search_result_xpath),"Cannot find search result with title '"+title+"' and description '"+description+"'");
+           String search_result_xpath=getResultSearchElement(substring);
+           this.waitForElementPresent(By.xpath(search_result_xpath),"Cannot find search result with substring "+substring);
+       }
+
+       public void clickByArticleWithSubstring(String substring)
+       {
+           String search_result_xpath=getResultSearchElement(substring);
+           this.waitForElementAndClick(By.xpath(search_result_xpath),"Cannot find and click search result with substring "+substring, 10);
+       }
+
+       public int getAmountOfFoundArticles()
+
+       {
+          //String search_result_locator="//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Linkin Park discography']";
+           //System.out.println(search_result_locator);
+           this.waitForElementPresent(
+                   By.xpath(SEARCH_RESULT_ELEMENT) ,
+                   "Cannot find anything by the request ",
+                   15
+           ) ;
+           return this.getAmountOfElements(By.xpath(SEARCH_RESULT_ELEMENT) ) ;
+
+       }
+
+        public void waitForEmptyForResultsLabel ()
+       {
+           this.waitForElementPresent(By.xpath(SEARCH_EMPTY_RESULT_ELEMENT),"Cannot find element result element.",15);
+       }
+
+       public void assertThereIsNoResultOfSearch()
+       {
+           this.assertElementNotPresent(By.xpath(SEARCH_RESULT_ELEMENT), "We supposed not to find any results" );
+       }
+
+       public int getAmountOfFoundElementLists()
+       {
+          return this.testSizeListForElementFind(By.id(SEARCH_RESULT));
+
+       }
+
+       public void waitForSearchListsToDisappear()
+       {
+           this.waitForElementNotPresent(By.id(SEARCH_RESULT),"Search lists is still present!",15);
        }
 
    }
